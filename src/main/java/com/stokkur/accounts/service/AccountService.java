02500bb2b48403ba0378aceccd87872abfa2f7f5
David <m.david.corrales@gmail.com>
2022-07-16 17:55:48 -0500

    David - feature: hooks up service and repository

--- src/main/java/com/stokkur/accounts/service/AccountService.java
+++ src/main/java/com/stokkur/accounts/service/AccountService.java
@@ -1,13 +1,20 @@
 package com.stokkur.accounts.service;
 
 import com.stokkur.accounts.model.Account;
+import com.stokkur.accounts.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AccountService {
+    private AccountRepository repository;
+
+    public AccountService(AccountRepository repository) {
+        this.repository = repository;
+    }
+
     public List<Account> listAllAccounts() {
-        return null;
+        return repository.findAll();
     }
 }
---
+++ src/test/java/com/stokkur/accounts/service/AccountServiceTest.java
@@ -0,0 +1,25 @@
+package com.stokkur.accounts.service;
+
+import com.stokkur.accounts.model.Account;
+import com.stokkur.accounts.repository.AccountRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AccountServiceTest {
+    @Test
+    void shouldFindAllExistingAccounts() {
+        AccountRepository repository = mock(AccountRepository.class);
+        AccountService service = new AccountService(repository);
+        Account sampleAccount = new Account(RandomUtils.nextLong(), RandomStringUtils.randomAlphabetic(10));
+        List<Account> expectedAccounts = List.of(sampleAccount);
+        when(repository.findAll()).thenReturn(expectedAccounts);
+        assertThat(service.listAllAccounts()).containsAll(expectedAccounts);
+    }
+}
