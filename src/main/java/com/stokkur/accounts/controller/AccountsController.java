98db7c1f330166072aea46228ca382ab0156ee1a
David <m.david.corrales@gmail.com>
2022-07-16 16:52:43 -0500

    David - feature: adds initial endpoint

--- src/main/java/com/stokkur/accounts/controller/AccountsController.java
+++ src/main/java/com/stokkur/accounts/controller/AccountsController.java
@@ -1,4 +1,23 @@
 package com.stokkur.accounts.controller;
 
+import com.stokkur.accounts.model.Account;
+import com.stokkur.accounts.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
 public class AccountsController {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @GetMapping("/accounts")
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
 }
