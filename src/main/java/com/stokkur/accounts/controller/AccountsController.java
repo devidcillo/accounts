5d3442aae2916903940b2988d3b1f1205bbc92fd
David <m.david.corrales@gmail.com>
2022-07-16 17:44:30 -0500

    David - lint: cleans imports and spacing

--- src/main/java/com/stokkur/accounts/controller/AccountsController.java
+++ src/main/java/com/stokkur/accounts/controller/AccountsController.java
@@ -1,9 +1,7 @@
 package com.stokkur.accounts.controller;
 
 import com.stokkur.accounts.model.Account;
-import com.stokkur.accounts.repository.AccountRepository;
 import com.stokkur.accounts.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/stokkur/accounts/controller/AccountsControllerTest.java
+++ src/test/java/com/stokkur/accounts/controller/AccountsControllerTest.java
@@ -12,7 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
 public class AccountsControllerTest {
 
     @Test
