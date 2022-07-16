cf073bd373b15588f313553c93d8e18f1cdd19f4
David <m.david.corrales@gmail.com>
2022-07-16 16:52:16 -0500

    David - feature: adds repository interface

---
+++ src/main/java/com/stokkur/accounts/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.stokkur.accounts.repository;
+
+import com.stokkur.accounts.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
