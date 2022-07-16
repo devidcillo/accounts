9c12fbeec823e9806b081763b19c599f52718e2a
David <m.david.corrales@gmail.com>
2022-07-16 16:23:09 -0500

    David - feature: adds account model

---
+++ src/main/java/com/stokkur/accounts/model/Account.java
@@ -0,0 +1,38 @@
+package com.stokkur.accounts.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Account() {
+    }
+
+    public Account(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
