404a5820a446bab0ef233091741f890244f556f7
David <m.david.corrales@gmail.com>
2022-07-17 15:09:29 -0500

    David - refactor: tweaks exception handler messaging

--- src/test/java/com/stokkur/accounts/controller/RestExceptionHandlerTest.java
+++ src/test/java/com/stokkur/accounts/controller/RestExceptionHandlerTest.java
@@ -2,22 +2,20 @@ package com.stokkur.accounts.controller;
 
 import com.stokkur.accounts.exception.NoSuchEntityException;
 import com.stokkur.accounts.response.ErrorResponse;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
-import javax.servlet.http.HttpServletRequest;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 class RestExceptionHandlerTest {
 
     @Test
     void shouldHandleNoSuchEntityExceptions() {
         RestExceptionHandler handler = new RestExceptionHandler();
-        NoSuchEntityException exception = mock(NoSuchEntityException.class);
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        ResponseEntity<ErrorResponse> response = handler.handleNoSuchEntityException(exception, request);
-        assertThat(response.getBody().getMessage()).isEqualTo("Not Found");
+        NoSuchEntityException exception = new NoSuchEntityException(RandomStringUtils.randomAlphanumeric(20));
+        ResponseEntity<ErrorResponse> response = handler.handleNoSuchEntityException(exception);
+        assertThat(response.getBody().getMessage()).isEqualTo(exception.getReason());
+        assertThat(response.getStatusCode()).isEqualTo(exception.getStatus());
     }
 }
