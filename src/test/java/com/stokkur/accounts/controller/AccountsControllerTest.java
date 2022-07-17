43ce6e84ea0c2afdf1e693c84506c09ce8aa9bc3
David <m.david.corrales@gmail.com>
2022-07-16 20:53:28 -0500

    David - refactor: Controller tests refactor

--- src/test/java/com/stokkur/accounts/controller/AccountsControllerTest.java
+++ src/test/java/com/stokkur/accounts/controller/AccountsControllerTest.java
@@ -5,6 +5,7 @@ import com.stokkur.accounts.request.AccountRequest;
 import com.stokkur.accounts.response.AccountResponse;
 import com.stokkur.accounts.service.AccountService;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -15,20 +16,25 @@ import static org.mockito.Mockito.when;
 
 public class AccountsControllerTest {
 
+    private AccountService service;
+    private AccountsController controller;
+
+    @BeforeEach
+    void setUp() {
+        service = mock(AccountService.class);
+        controller = new AccountsController(service);
+    }
+
     @Test
     void shouldListAllAccounts() {
-        AccountService accountService = mock(AccountService.class);
-        AccountsController controller = new AccountsController(accountService);
         Account sampleAccount = new Account(RandomStringUtils.randomAlphabetic(10));
         List<Account> expectedAccounts = List.of(sampleAccount);
-        when(accountService.listAllAccounts()).thenReturn(expectedAccounts);
+        when(service.listAllAccounts()).thenReturn(expectedAccounts);
         assertThat(controller.getAllAccounts()).containsAll(expectedAccounts);
     }
 
     @Test
     void shouldAddAccountGivenUserInformation() {
-        AccountService service = mock(AccountService.class);
-        AccountsController controller = new AccountsController(service);
         AccountRequest accountRequest = new AccountRequest(RandomStringUtils.randomAlphabetic(10));
         Account account = accountRequest.toAccount();
         when(service.addAccount(accountRequest)).thenReturn(account);
