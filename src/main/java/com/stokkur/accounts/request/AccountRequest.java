2e128d92ceda750a1abe8d047290edea7b22e676
David <m.david.corrales@gmail.com>
2022-07-17 20:46:08 -0500

    David - fix: marks username and password as required

--- src/main/java/com/stokkur/accounts/request/AccountRequest.java
+++ src/main/java/com/stokkur/accounts/request/AccountRequest.java
@@ -5,18 +5,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.stokkur.accounts.model.Account;
 
 public class AccountRequest {
-    private final String name;
+    private final String username;
     private final String password;
 
     @JsonCreator
     public AccountRequest(
-            @JsonProperty("name") String name,
-            @JsonProperty("password") String password) {
-        this.name = name;
+            @JsonProperty(value = "username", required = true) String username,
+            @JsonProperty(value = "password", required = true) String password) {
+        this.username = username;
         this.password = password;
     }
 
     public Account toAccount() {
-        return new Account(name, password);
+        return new Account(username, password);
     }
 }
